904d786c5f33147435dd3984692d4ca82bf99d65
janithwanni <r.t.doram@gmail.com>
2018-09-06 00:26:32 +0530

    login ui and login routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  //{ path: "", pathMatch: "full", loadChildren:"app/preliminary-module/preliminary-module.module#PreliminaryModuleModule" },
+  {
+    path: "",
+    pathMatch: "full",
+    loadChildren:
+      "preliminary-module/preliminary-module.module#PreliminaryModuleModule"
+  }
 ];
 
 @NgModule({
--- src/app/preliminary-module/preliminary-module-routing.module.ts
+++ src/app/preliminary-module/preliminary-module-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginDialogCompComponent } from "./login-dialog-comp/login-dialog-comp.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: LoginDialogCompComponent, pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class PreliminaryModuleRoutingModule { }
+export class PreliminaryModuleRoutingModule {}
