86501d04694d7a7a4ca766b569bf4e9ea56555e3
Janith Wanniarachchi <r.t.doram@gmail.com>
2018-09-16 13:32:35 +0530

    angular schematics generate table bitch!

--- src/app/overview-module/leaderboard-table/leaderboard-table-datasource.ts
+++ src/app/overview-module/leaderboard-table/leaderboard-table-datasource.ts
@@ -1,7 +1,7 @@
-import { DataSource } from '@angular/cdk/collections';
-import { MatPaginator, MatSort } from '@angular/material';
-import { map } from 'rxjs/operators';
-import { Observable, of as observableOf, merge } from 'rxjs';
+import { DataSource } from "@angular/cdk/collections";
+import { MatPaginator, MatSort } from "@angular/material";
+import { map } from "rxjs/operators";
+import { Observable, of as observableOf, merge } from "rxjs";
 
 // TODO: Replace this with your own data model type
 export interface LeaderboardTableItem {
@@ -11,26 +11,26 @@ export interface LeaderboardTableItem {
 
 // TODO: replace this with real data from your application
 const EXAMPLE_DATA: LeaderboardTableItem[] = [
-  {id: 1, name: 'Hydrogen'},
-  {id: 2, name: 'Helium'},
-  {id: 3, name: 'Lithium'},
-  {id: 4, name: 'Beryllium'},
-  {id: 5, name: 'Boron'},
-  {id: 6, name: 'Carbon'},
-  {id: 7, name: 'Nitrogen'},
-  {id: 8, name: 'Oxygen'},
-  {id: 9, name: 'Fluorine'},
-  {id: 10, name: 'Neon'},
-  {id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},
+  { id: 1, name: "Hydrogen" },
+  { id: 2, name: "Helium" },
+  { id: 3, name: "Lithium" },
+  { id: 4, name: "Beryllium" },
+  { id: 5, name: "Boron" },
+  { id: 6, name: "Carbon" },
+  { id: 7, name: "Nitrogen" },
+  { id: 8, name: "Oxygen" },
+  { id: 9, name: "Fluorine" },
+  { id: 10, name: "Neon" },
+  { id: 11, name: "Sodium" },
+  { id: 12, name: "Magnesium" },
+  { id: 13, name: "Aluminum" },
+  { id: 14, name: "Silicon" },
+  { id: 15, name: "Phosphorus" },
+  { id: 16, name: "Sulfur" },
+  { id: 17, name: "Chlorine" },
+  { id: 18, name: "Argon" },
+  { id: 19, name: "Potassium" },
+  { id: 20, name: "Calcium" }
 ];
 
 /**
@@ -38,7 +38,9 @@ const EXAMPLE_DATA: LeaderboardTableItem[] = [
  * encapsulate all logic for fetching and manipulating the displayed data
  * (including sorting, pagination, and filtering).
  */
-export class LeaderboardTableDataSource extends DataSource<LeaderboardTableItem> {
+export class LeaderboardTableDataSource extends DataSource<
+  LeaderboardTableItem
+> {
   data: LeaderboardTableItem[] = EXAMPLE_DATA;
 
   constructor(private paginator: MatPaginator, private sort: MatSort) {
@@ -61,10 +63,12 @@ export class LeaderboardTableDataSource extends DataSource<LeaderboardTableItem>
 
     // Set the paginators length
     this.paginator.length = this.data.length;
-
-    return merge(...dataMutations).pipe(map(() => {
-      return this.getPagedData(this.getSortedData([...this.data]));
-    }));
+    this.paginator.pageSize = 10;
+    return merge(...dataMutations).pipe(
+      map(() => {
+        return this.getPagedData(this.getSortedData([...this.data]));
+      })
+    );
   }
 
   /**
@@ -87,16 +91,19 @@ export class LeaderboardTableDataSource extends DataSource<LeaderboardTableItem>
    * this would be replaced by requesting the appropriate data from the server.
    */
   private getSortedData(data: LeaderboardTableItem[]) {
-    if (!this.sort.active || this.sort.direction === '') {
+    if (!this.sort.active || this.sort.direction === "") {
       return data;
     }
 
     return data.sort((a, b) => {
-      const isAsc = this.sort.direction === 'asc';
+      const isAsc = this.sort.direction === "asc";
       switch (this.sort.active) {
-        case 'name': return compare(a.name, b.name, isAsc);
-        case 'id': return compare(+a.id, +b.id, isAsc);
-        default: return 0;
+        case "name":
+          return compare(a.name, b.name, isAsc);
+        case "id":
+          return compare(+a.id, +b.id, isAsc);
+        default:
+          return 0;
       }
     });
   }
--- src/app/overview-module/leaderboard-table/leaderboard-table.component.ts
+++ src/app/overview-module/leaderboard-table/leaderboard-table.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatSort } from '@angular/material';
-import { LeaderboardTableDataSource } from './leaderboard-table-datasource';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator, MatSort } from "@angular/material";
+import { LeaderboardTableDataSource } from "./leaderboard-table-datasource";
 
 @Component({
-  selector: 'app-/overview-module/leaderboard-table',
-  templateUrl: './leaderboard-table.component.html',
-  styleUrls: ['./leaderboard-table.component.css']
+  selector: "app-leaderboard-table",
+  templateUrl: "./leaderboard-table.component.html",
+  styleUrls: ["./leaderboard-table.component.css"]
 })
 export class LeaderboardTableComponent implements OnInit {
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
+  @ViewChild(MatSort)
+  sort: MatSort;
   dataSource: LeaderboardTableDataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ["id", "name"];
 
   ngOnInit() {
     this.dataSource = new LeaderboardTableDataSource(this.paginator, this.sort);
